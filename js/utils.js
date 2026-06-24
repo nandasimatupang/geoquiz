3ce91744babde2d085fcae822b54749e4f23ef05
Nanda <nanda@example.com>
2026-06-24 02:22:28 -0700

    fix: make normalize more forgiving for hyphens and spaces

--- js/utils.js
+++ js/utils.js
@@ -6,7 +6,9 @@ function normalize(str) {
   return str
     .normalize('NFD')
     .replace(/[\u0300-\u036f]/g, '')
-    .replace(/[^a-zA-Z\s'-]/g, '')
+    .replace(/-/g, ' ')
+    .replace(/[^a-zA-Z\s']/g, '')
+    .replace(/\s+/g, ' ')
     .trim()
     .toLowerCase();
 }
